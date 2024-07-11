9eedb7f8c180104ce2563b3c9d052fccf3e49a79
pedrodruviaro <pedrodruviaro@gmail.com>
2024-07-11 19:28:25 -0300

    feat: build ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  nitro: {
-    static: true,
-  },
   devtools: { enabled: true },
   modules: [
     "@nuxtjs/tailwindcss",
